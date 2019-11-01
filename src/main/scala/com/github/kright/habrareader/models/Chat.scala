2dfaa0093f960f057cf33a9540c5cc399493b0ff
Igor Slobodskov <igor.slobodskov@phystech.edu>
2019-11-01 11:27:10 +0300

    rm unused lastUpdateDate in chat

--- src/main/scala/com/github/kright/habrareader/models/Chat.scala
+++ src/main/scala/com/github/kright/habrareader/models/Chat.scala
@@ -1,15 +1,10 @@
 package com.github.kright.habrareader.models
 
-import java.util.Date
-
-import com.github.kright.habrareader.utils.DateUtils
-import com.github.kright.habrareader.utils.DateUtils._
 import io.circe._
 import io.circe.syntax._
 
 
 case class Chat(id: Long,
-                lastUpdateDate: Date,
                 filterSettings: FilterSettings,
                 sentArticles: Map[Int, SentArticle] = Map.empty) {
 
@@ -29,12 +24,11 @@ case class Chat(id: Long,
 
 object Chat {
   def withDefaultSettings(id: Long) =
-    Chat(id, DateUtils.now, filterSettings = FilterSettings(ratingThreshold = 25.0))
+    Chat(id, filterSettings = FilterSettings(ratingThreshold = 25.0))
 
   implicit val encoder: Encoder[Chat] = (chat: Chat) =>
     Json.obj(
       "id" := chat.id,
-      "lastUpdateDate" := chat.lastUpdateDate,
       "filterSettings" := chat.filterSettings,
       "sentPosts" := chat.sentArticles,
     )
@@ -42,9 +36,8 @@ object Chat {
   implicit val decoder: Decoder[Chat] = (c: HCursor) => {
     for {
       id <- c.get[Long]("id")
-      lastUpdateDate <- c.get[Date]("lastUpdateDate")
       filterSettings <- c.get[FilterSettings]("filterSettings")
       sentPosts <- c.get[Map[Int, SentArticle]]("sentPosts")
-    } yield Chat(id, lastUpdateDate, filterSettings, sentPosts)
+    } yield Chat(id, filterSettings, sentPosts)
   }
 }
